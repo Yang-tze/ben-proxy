bc791583dd55729db96796272c4f87fe30ef3b26
Tony <tony.kijak@gmail.com>
2018-08-02 20:58:20 -0700

    adds proxy routing

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(proxy({
+  router: {
+    '/related/**' : 'http://localhost:3001',
+    '/related/**' : 'http://localhost:3002',
+    '/products/**' : 'http://localhost:3003',
+    '/reviews/**' : 'http://localhost:3004',
+  }
+}));
 
 app.get('/', (req, res) => {
   res.redirect('/1');
