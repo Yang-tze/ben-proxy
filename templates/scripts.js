573814a5ee2ad8dd5fcdac41abcdee6280d5eca4
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-17 16:36:26 -0700

    Use minified react bundles

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,6 +1,6 @@
 module.exports = (items, props) => `
-  <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
-  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
   ${items
     .map(item => {
       return `<script src="/services/${item}.js"></script>`;
