6bae2e7f84d0794f09454ba107f2568e981d5e36
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-20 16:14:53 -0700

    Use morgan logging and clean up npm scripts

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const apiConfig = require("./api-config.json");
