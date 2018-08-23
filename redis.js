b5d506a9e8c51eb8fda687780ac0720d976f6146
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-23 15:11:46 -0700

    Allow for REDIS_HOST env variable

--- redis.js
+++ redis.js
@@ -1,6 +1,8 @@
 const redis = require("redis");
 
-const client = redis.createClient();
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || "127.0.0.1"
+});
 
 client.on("error", err => {
   console.log(err);
